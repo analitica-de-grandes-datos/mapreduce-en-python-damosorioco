8023d9e8cf60dfd9f992b008a2181e9ed1b836c9
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-25 12:08:54 -0500

    P3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if _name_ == "_main_":
+
+# itera sobre cada linea de codigo recibida
+# a traves del flujo de entrada
+
+  for line in sys.stdin:
+
+    #Eliminar vacíos
+    clean = line.strip()
+
+    col = clean.split(',')
+    col_1 = col[0]
+    col_2 = col[1]
+
+  # escribe al flujo estandar de salida
+
+    # print ('%s, %s' %  (col_1, col_2))
+    print ('%s\t%s' % (col_1, col_2))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import operator
+if _name_ == '_main_':
+    dict_from_list = {}
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+        
+
+        dict_from_list[key] = val
+
+        sorted_= sorted(dict_from_list.items(), key=operator.itemgetter(1))
+        # keys_dict = sorted_.keys()
+        # sorted_list = sorted(dict_from_list.items(), key=lambda x: x[1])
+        # complete = key + "," + val
+
+    for elemento in sorted_:
+        sys.stdout.write("{},{}\n".format(elemento[0], elemento[1]))
+    # print(sorted_)

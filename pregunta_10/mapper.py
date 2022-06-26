3ac67f764dd0e076c445b0a75f4e557c42b9da48
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-26 16:56:26 -0500

    P10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+from dataclasses import fields
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+
+        fields = line.strip().split("\t")
+        value = fields[0]
+        letter = fields[1]
+
+    sys-sys.stdout.write("{}\t{}\n".format(value, letter))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,34 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from ntpath import join
+import sys
+
+if __name__ == "__main__":
+
+    letters = dict()
+
+    for line in sys.stdin:
+
+        value, letter = line.split("\t")
+        letter = letter.strip().split(",")
+
+        for x in letter:
+
+            if x not in letters.keys():
+
+                letters[x] = value
+
+            else:
+
+                letters[x] = letters[x] + ',' + value
+
+        tuple = sorted(letters.items(), key = lambda y: y[0])
+
+    for letter1, number_related in tuple:
+
+        unord_number = number_related.split()
+        ord_number = sorted(unord_number, key=int)
+        ord_number = ',', join(ord_number)
+
+        sys.stdout.write("{}\t{}\n".format(letter1, ord_number))

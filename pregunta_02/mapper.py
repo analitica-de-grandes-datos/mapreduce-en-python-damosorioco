e7bb97b65d342277814ee1df0dac7b8a0b5ce993
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-26 11:11:33 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,14 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 
-import sys 
-
+import sys
 if __name__ == "__main__":
 
-    for row in sys.stdin:
+    for line in sys.stdin:
+        fields = line.strip().split(',',5)
+        purpose =  fields[3]
+        amount = fields[4]
 
-        campos = row.strip().split(',',5)
-        purpose = campos[3]
-        amount = campos[4]
-
-    sys.stdout.write("{}\t{}\n".format(purpose, amount))
+        sys.stdout.write("{}\t{}\n".format(purpose, amount))

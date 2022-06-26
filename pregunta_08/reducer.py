892275197aff5ad668affaf3d91966aa99572049
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-26 15:50:54 -0500

    P8

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,39 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+from numpy import mean
+
+if __name__ == "__main__":
+
+    curkey = None
+    list_value = []
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+
+            list_value.append(val)
+
+        else:
+
+            if curkey is not None:
+
+                suma = sum(list_value)
+                avg = mean(list_value)
+                list_value.clear()
+                
+                sys-sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, avg))
+
+            curkey = key
+            list_value.append(val)
+
+    suma = sum(list_value)
+    avg = mean(list_value)
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, avg))
+

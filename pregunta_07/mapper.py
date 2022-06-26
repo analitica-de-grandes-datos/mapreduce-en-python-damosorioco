e95dc8de0ee27bb43f0fb21460c23931d9eda3c1
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-26 15:31:43 -0500

    upgrade P7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -10,9 +10,8 @@ if __name__ == '__main__':
     for line in sys.stdin:
 
         fields = line.strip().split('   ')
-
         letter = fields[0]
         date = fields[1]
         value = fields[2]
 
-        sys.stdout.write('{}\t{}t{}\n'.format(letter, date, value))
+        sys.stdout.write("{}\t{}t{}\n".format(letter, date, value))

ea5cfd13a774bcbb4ca256983d8e3fb3c5e5f8c9
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-25 23:49:28 -0500

    P2

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,31 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split('\t')
+        val = int(val)
+
+        if key == curkey:
+
+            if val > total:
+
+                total = val
+
+            else:
+
+                if curkey is not None:
+
+                    sys.stdin.write("{}\t{}\n".format(curkey, total))
+
+                curkey = total
+                total = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, total))

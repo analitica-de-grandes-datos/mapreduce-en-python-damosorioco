666954e581d87c7579dc34cb5f96d3c57bddb809
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-26 14:58:49 -0500

    P7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+from dataclasses import field, fields
+import sys
+
+if __name__ == '__main__':
+
+    for line in sys.stdin:
+
+        fields = line.strip().split('   ')
+
+        letter = fields[0]
+        values = fields [2]
+        dates = fields[1]
+
+        sys.stdout.write('{}\t{}t{}\n'.format(letter, dates, values))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    list_tuples = []
+
+    for line in sys.stdin:
+
+        key, dates, values = line.split('\t')
+
+        values = int(values)
+        tuple =  (key, dates, values)
+        list_tuples.append(tuple)
+
+    list_tuples = sorted(list_tuples, key=lambda x: (x[0], x[2]))
+
+    for tuple in list_tuples:
+
+        sys.stdout.write('{}  {}   {}\n'.format(tuple[0], tuple [1], tuple[2]))

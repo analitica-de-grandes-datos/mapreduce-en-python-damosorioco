789fc9ba5a837e70278f19f6065ce06de60727f8
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-26 13:41:30 -0500

    P5

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+from calendar import month
+from dataclasses import fields
+import sys
+
+if __name__ == '__main__':
+
+    for line in sys.stdin:
+
+        fields = line.strip().split('   ')
+        dates = fields[1]
+        split_dates = dates.split('-')
+        month = split_dates[1]
+
+        sys.stdout.write("{}\t\n".format(month))

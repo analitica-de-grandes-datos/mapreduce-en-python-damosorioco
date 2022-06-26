19e30cdb471a7dbaba11c26053ac835d8dfcfdd9
Danilo Osorio <damosorioco@unal.edu.co>
2022-06-26 12:02:14 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,14 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+from dataclasses import fields
 import sys
 
 if __name__=='__main__':
 
     for line in sys.stdin:
 
-        campos = line.strip().split('   ')
-        letter = campos[0]
+        fields = line.strip().split('   ')
+        letter = fields[0]
 
         sys.stdin.write("{}\t1\n".format(letter))
-        
+
